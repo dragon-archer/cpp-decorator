b4483846ce3fadb750b090a59f09bd898b8b8941
dragon-archer <dragon-archer@outlook.com>
2020-04-18 10:15:29 +0800

    Add main file
    Add `src/main.cpp`

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file    src/main.cpp
+ * @author  dragon-archer (dragon-archer@outlook.com)
+ * @brief   The entry point of the program
+ * @version 0.0.1
+ *
+ * @license This file is published under The MIT License
+ *          See License in the root directory for details
+ *          Copyright (c) 2020 dragon-archer
+ */
+
+#include <pch.h>
+
+/**
+ * @brief  The entry point of the program
+ * @param  argc number of arguments passed by cmdline 
+ * @param  argv arguments
+ * @return int  program status
+ */
+int main(int argc, char* argv[])
+{
+	return 0;
+}
