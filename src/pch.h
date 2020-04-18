36f0faacb247ad64ffc57229c8d75d0ced0ec4d8
dragon-archer <dragon-archer@outlook.com>
2020-04-18 10:14:36 +0800

    Add precompiled header
    Add `src/pch.h`

---
+++ src/pch.h
@@ -0,0 +1,23 @@
+/**
+ * @file    src/pch.h
+ * @author  dragon-archer (dragon-archer@outlook.com)
+ * @brief   The precompiled header
+ * @version 0.0.1
+ * 
+ * @license This file is published under The MIT License
+ *          See License in the root directory for details
+ *          Copyright (c) 2020 dragon-archer
+ */
+
+#ifndef _CPPDECORATOR_PCH_H
+#define _CPPDECORATOR_PCH_H 1
+
+#ifdef _MSC_VER
+#pragma once
+#endif /* _MSC_VER */
+
+// Include C++ standard headers
+#include <iostream>
+#include <string>
+
+#endif /* _CPPDECORATOR_PCH_H */
