0b4a145f9061e82abbe52205f155b07a2d2f2b3e
dragon-archer <dragon-archer@outlook.com>
2020-04-19 21:21:57 +0800

    Add utility functions
    Add print_usage()

---
+++ src/utility.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file    src/utility.cpp
+ * @author  dragon-archer (dragon-archer@outlook.com)
+ * @brief 	The implements of utility functions for the program
+ * @version 0.0.1
+ * 
+ * @license This file is published under The MIT License
+ *          See License in the root directory for details
+ *          Copyright (c) 2020 dragon-archer
+ */
+
+#include <utility.h>
+
+/**
+ * @brief Print usage
+ * @param progname The name of the program (aka argv[0])
+ */
+void print_usage(const char* progname)
+{
+	printf(R"(
+Usage: %s <options> <function>
+
+Options:
+	-u, --undecorate  Undecorate <function>
+	-d, --decorate    Decorate <function>
+	-h, --help        Print this help message
+Function:
+	The function name(decorated or not), 
+	it can be quoted, or just "as is".
+	Some of the modifiers can be omitted.
+	The undecorated function looks like this:
+	<calling convention> <declartor> (<parameters>)
+
+	<calling convention>: optional, default is __cdecl
+	<declartor>:          required
+	<parameters>:         optional, split with ','
+Return value:
+	If no error occared, return 0
+	If the option is invalid, return 1
+	If the funcion is invalid, return 2
+	Otherwise, return 3
+)", progname);
+}
---
+++ src/utility.h
@@ -0,0 +1,27 @@
+/**
+ * @file    src/utility.h
+ * @author  dragon-archer (dragon-archer@outlook.com)
+ * @brief 
+ * @version 0.0.1
+ * 
+ * @license This file is published under The MIT License
+ *          See License in the root directory for details
+ *          Copyright (c) 2020 dragon-archer
+ */
+
+#ifndef _CPPDECORATOR_UTILITY_H
+#define _CPPDECORATOR_UTILITY_H 1
+
+#ifdef _MSC_VER
+#pragma once
+#endif /* _MSC_VER */
+
+#include <pch.h>
+
+/**
+ * @brief Print usage
+ * @param progname The name of the program (aka argv[0])
+ */
+void print_usage(const char* progname);
+
+#endif /* _CPPDECORATOR_UTILITY_H */
